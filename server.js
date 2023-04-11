cfd13d2c810e2be34e171cc2f2871bfa87b9db07
bachtiar <bachtiaralif41@gmail.com>
2023-04-11 17:30:04 +0700

    .

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/dir", (req, res) => {
+  res.send(__dirname);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
