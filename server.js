bf5b7e9590e981e85ebe321db62ca8e3c9a8edbc
Bachtiar alif darmawan <109453337+dapkniht@users.noreply.github.com>
2023-06-11 11:49:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello Worlld");
+  res.send("Hello Worlld"+process.env.TEST);
 });
 
 app.get("/dir", (req, res) => {
